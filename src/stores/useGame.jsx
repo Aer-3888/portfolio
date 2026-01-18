3eb469510fbc376af6f928216feccae5601c39d1
Aer-3888 <theo.phan.quoc.huy@gmail.com>
2026-01-18 22:08:29 +0100

    feat(camera): implement optimized third-person camera

--- src/stores/useGame.jsx
+++ src/stores/useGame.jsx
@@ -8,9 +8,14 @@ export default create(subscribeWithSelector((set) => ({
     // Is the interface open?
     isInterfaceOpen: false,
 
+    // What interactable object is nearby? (null = nothing, or {name, data})
+    nearbyObject: null,
+
     // Actions to change the state
     focusOn: (objectName) => set({ focusedObject: objectName }),
     clearFocus: () => set({ focusedObject: null }),
     openInterface: () => set({ isInterfaceOpen: true }),
     closeInterface: () => set({ isInterfaceOpen: false }),
+    setNearbyObject: (obj) => set({ nearbyObject: obj }),
+    clearNearbyObject: () => set({ nearbyObject: null }),
 })))
