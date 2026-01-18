fa925de0a573793416b321960f0db3a2c7bc1fc5
Theo PHAN <e238665t@etu.univ-nantes.fr>
2026-01-18 14:07:12 +0100

    feat(player): add physics and third-person character controls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,26 +1,76 @@
-import { OrbitControls } from '@react-three/drei'
+import { Physics, RigidBody, CuboidCollider } from '@react-three/rapier'
+import Ecctrl from 'ecctrl'
 
 export default function Experience() {
   return (
     <>
-      {/* 1. Camera Controls: Allows you to rotate the view with the mouse */}
-      <OrbitControls makeDefault />
-
-      {/* 2. Lighting: Essential to see 3D objects */}
+      {/* Lights */}
       <ambientLight intensity={0.5} />
-      <directionalLight position={[10, 10, 5]} intensity={1} />
+      <directionalLight 
+        position={[10, 10, 10]} 
+        intensity={1} 
+        castShadow 
+      />
 
-      {/* 3. The Floor (The Stage) */}
-      <mesh rotation-x={-Math.PI / 2} position-y={-1}>
-        <planeGeometry args={[20, 20]} />
-        <meshStandardMaterial color="#444" />
-      </mesh>
+      {/* The Physics World (Gravity is on!) */}
+      {/* debug={true} lets you see the invisible hitboxes (wireframes) */}
+        <Physics debug={true} timeStep="vary">        
+        {/* PLAYER CHARACTER */}
+        <Ecctrl 
+          position={[0, 1, 0]}
+          camInitDis={-5} // Camera distance
+          camMaxDis={-5} 
+          maxVelLimit={5} 
+          jumpVel={4}
+        >
+          {/* This is the visual representation of "You" (a floating capsule for now) */}
+          <mesh castShadow position-y={0.5}>
+            <capsuleGeometry args={[0.3, 1]} />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+        </Ecctrl>
 
-      {/* 4. A Placeholder Cube (Representing an NPC or Object) */}
-      <mesh position={[0, 0, 0]}>
-        <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
-      </mesh>
+        {/* THE STAGE (Floor & Walls) */}
+        {/* type="fixed" means it won't move when you bump into it */}
+        <RigidBody type="fixed" friction={2}>
+          {/* Simple invisible collider for the floor so Ecctrl can detect ground for jumping */}
+          <CuboidCollider args={[10, 0.05, 10]} position={[0, -1.01, 0]} />
+          
+          {/* Floor */}
+          <mesh receiveShadow position-y={-1} rotation-x={-Math.PI / 2}>
+            <planeGeometry args={[20, 20]} />
+            <meshStandardMaterial color="#333" />
+          </mesh>
+
+          {/* Wall 1 (Back) */}
+          <mesh receiveShadow position={[0, 1, -10]}>
+            <boxGeometry args={[20, 4, 1]} />
+            <meshStandardMaterial color="#555" />
+          </mesh>
+          
+          {/* Wall 2 (Right) */}
+          <mesh receiveShadow position={[10, 1, 0]} rotation-y={-Math.PI / 2}>
+            <boxGeometry args={[20, 4, 1]} />
+            <meshStandardMaterial color="#555" />
+          </mesh>
+
+           {/* Wall 3 (Left) */}
+           <mesh receiveShadow position={[-10, 1, 0]} rotation-y={-Math.PI / 2}>
+            <boxGeometry args={[20, 4, 1]} />
+            <meshStandardMaterial color="#555" />
+          </mesh>
+
+        </RigidBody>
+
+        {/* An Obstacle to jump over */}
+          <RigidBody position={[3, 0, 3]}>
+           <mesh castShadow>
+             <boxGeometry />
+             <meshStandardMaterial color="orange" />
+           </mesh>
+        </RigidBody>
+
+      </Physics>
     </>
   )
 }
