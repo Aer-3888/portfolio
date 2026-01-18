74ab4bfc0d66b511ea0763ae72d09f435c74c3c1
Aer-3888 <theo.phan.quoc.huy@gmail.com>
2026-01-18 14:07:12 +0100

    feat(player): add physics and third-person character controls

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import { Canvas } from '@react-three/fiber'
+import { KeyboardControls } from '@react-three/drei'
 import Experience from './Experience'
 
 function App() {
+  const keyboardMap = [
+    { name: 'forward', keys: ['ArrowUp', 'KeyW'] },
+    { name: 'backward', keys: ['ArrowDown', 'KeyS'] },
+    { name: 'leftward', keys: ['ArrowLeft', 'KeyA'] },
+    { name: 'rightward', keys: ['ArrowRight', 'KeyD'] },
+    { name: 'jump', keys: ['Space'] },
+    { name: 'run', keys: ['Shift'] },
+  ]
+
   return (
-    // The Canvas is the parent component for all 3D content
-    <Canvas
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [4, 2, 6] // Moves camera back so we can see the scene
-      }}
-    >
-      <Experience />
-    </Canvas>
+    <KeyboardControls map={keyboardMap}>
+      <Canvas
+        shadows
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [2.5, 4, 6]
+        }}
+      >
+        <Experience />
+      </Canvas>
+    </KeyboardControls>
   )
 }
 
