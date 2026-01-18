33d28808c834fe68c80ec2938c453a3231a71232
Aer-3888 <theo.phan.quoc.huy@gmail.com>
2026-01-18 18:51:52 +0100

    feat(player): add boxy bot character with ecctrl physics and jumping

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,5 +1,5 @@
 import { Physics, RigidBody, CuboidCollider } from '@react-three/rapier'
-import Ecctrl from 'ecctrl'
+import Player from './components/Player'
 import Laptop from './components/Laptop'
 import Desk from './components/Desk'
 import Chair from './components/Chair'
@@ -16,30 +16,14 @@ export default function Experience() {
         castShadow 
       />
 
-      {/* The Physics World (Gravity is on!) */}
-      {/* debug={true} lets you see the invisible hitboxes (wireframes) */}
-        <Physics debug={true} timeStep="vary">        
-        {/* PLAYER CHARACTER */}
-        <Ecctrl 
-          position={[0, 1, 0]}
-          camInitDis={-5} // Camera distance
-          camMaxDis={-5} 
-          maxVelLimit={5} 
-          jumpVel={4}
-          capsuleRadius={0.3}
-          capsuleHalfHeight={0.5}
-        >
-          {/* This is the visual representation of "You" (a floating capsule for now) */}
-          <mesh castShadow position-y={0.5}>
-            <capsuleGeometry args={[0.3, 1]} />
-            <meshStandardMaterial color="mediumpurple" />
-          </mesh>
-        </Ecctrl>
+      {/* The Physics World */}
+      <Physics debug={true} timeStep="vary">        
+        
+        <Player position={[0, 3, 0]} />
 
         {/* THE STAGE (Floor & Walls) */}
-        {/* type="fixed" means it won't move when you bump into it */}
         <RigidBody type="fixed" friction={2}>
-          {/* Simple invisible collider for the floor so Ecctrl can detect ground for jumping */}
+          {/* Simple invisible collider for the floor so Ecctrl can detect ground */}
           <CuboidCollider args={[10, 0.05, 10]} position={[0, -1.01, 0]} />
           
           {/* Floor */}
@@ -75,16 +59,20 @@ export default function Experience() {
              <meshStandardMaterial color="orange" />
            </mesh>
         </RigidBody>
+
         <Desk scale={1.8} position={[5, -1, -5]} rotation-y={-Math.PI * 0.5} />
+        
         <Laptop 
             scale={0.08} 
             position={[5, 0.7, -5]}  
         />
+        
         <Chair 
             scale={0.06} 
             position={[4.5, 1.2, -2.5]} 
             rotation-y={-Math.PI * 1.1} 
         />
+        
         <Project 
             title="Waiki" 
             description="A Flutter-based Android application that integrates with a physical device to combat screen addiction."
